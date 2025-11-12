af041cecd0aaf2417dc9e079fd54f20e12b25730
kawrae <coreyblack@hotmail.co.uk>
2025-11-12 19:03:31 +0000

    Colour tweak

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,8 +25,8 @@ export default {
       colors: {
         brand: {
           50:  "#fff1f2",
-          500: "#f43f5e",
-          600: "#e11d48",
+          500: "#d12055ff",
+          600: "#871336",
         },
       },
       boxShadow: {
