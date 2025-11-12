e691d8de057adbca7fba7be49b1b4573d3c68872
kawrae <coreyblack@hotmail.co.uk>
2025-11-12 01:13:09 +0000

    Added dark mode script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,20 +4,33 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title', 'Flash Site')</title>
+  <link rel="icon" type="image/svg+xml" href="/icon.svg">
 
   <link rel="preconnect" href="https://fonts.bunny.net">
   <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
   <link href="https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css" rel="stylesheet">
 
+  <script>
+    (function () {
+      const stored = localStorage.getItem('theme');
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      const dark = stored ? stored === 'dark' : prefersDark;
+      if (dark) document.documentElement.classList.add('dark');
+    })();
+  </script>
+
   @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body class="with-sticky @yield('body-class')">
+<body class="with-sticky @yield('body-class') min-h-screen flex flex-col">
 
   @include('partials.nav')
 
-  @yield('content')
+  <main id="main" class="flex-1">
+    @yield('content')
+  </main>
 
   @includeIf('partials.footer')
+
 </body>
 </html>
