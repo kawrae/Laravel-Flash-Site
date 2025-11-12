d3e82d476daf273274c578a47bdda84f3c8f9e9b
kawrae <coreyblack@hotmail.co.uk>
2025-11-12 01:12:06 +0000

    Added commissions page

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommissionsController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/gallery', [PageController::class, 'gallery'])->name('gallery.index');
 Route::get('/posts/{slug}', [PostController::class, 'show'])->name('post.show');
+Route::get('/commissions', [CommissionsController::class, 'create'])->name('commissions.create');
+Route::post('/commissions', [CommissionsController::class, 'store'])->name('commissions.store');
